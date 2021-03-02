305a4a4ff4fd965de4d88d18b6059ed8b1893415
wx0322 <wx_0322@163.com>
2021-03-02 18:17:01 +0800

    first commit

---
+++ xamarinTest.Android/obj/Debug/90/android/src/crc64e5ba8210f1f7566e/SplashScreen.java
@@ -0,0 +1,47 @@
+package crc64e5ba8210f1f7566e;
+
+
+public class SplashScreen
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("xamarinTest.Droid.SplashScreen, xamarinTest.Android", SplashScreen.class, __md_methods);
+	}
+
+
+	public SplashScreen ()
+	{
+		super ();
+		if (getClass () == SplashScreen.class)
+			mono.android.TypeManager.Activate ("xamarinTest.Droid.SplashScreen, xamarinTest.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
